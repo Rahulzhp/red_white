489d47fd6abd7e2216272c2705237836508f1a4d
Rahulzhp <akumari42872@gmail.com>
2023-06-27 13:28:05 +0530

    basic structure

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Allpages from "./AllPages"
+
+function App() {
+  return (
+    <div>
+      <Allpages />
+    </div>
+  );
+}
+
+export default App;
